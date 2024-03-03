a812edb918a0be8cf788e1dd54bda85c8ab9a6e9
Alina <astapchik.1993@mail.ru>
2024-03-04 00:33:09 +0100

    feat: add task VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,76 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor (isdirect) {
+    this.keys = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.isdirect = isdirect === false ? false : true
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(input, key) {
+    if (input === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    const inputArr = input.split('').map(item => {
+      const upperItem = item.toUpperCase();
+      return this.keys.indexOf(upperItem) !== -1 ? this.keys.indexOf(upperItem) : item;
+    });
+    const keyArr = key.split('').map(item => this.keys.indexOf(item.toUpperCase()))
+    const newSumArr = []
+    let keyCounter = 0
+    for (let i = 0; i < inputArr.length; i++) {
+      if(typeof inputArr[i] !== 'number') {
+        newSumArr[i] = inputArr[i]
+      } else {
+        if (keyCounter >= keyArr.length) {
+          keyCounter = 0
+        }
+        let sum= inputArr[i] + keyArr[keyCounter]
+        if (sum >= this.keys.length) {
+          sum = sum % this.keys.length
+        }
+        newSumArr[i] = sum;
+        keyCounter ++;
+      }
+    }
+
+    const output = newSumArr.map((item,index) => {
+      if (!isNaN(input[index]) && input[index] !== ' ') {
+        return input[index]
+      } else if (!(this.keys[item])) {
+        return item
+      } else 
+        return this.keys[item]
+    })
+
+    return this.isdirect ? output.join('') : output.reverse().join('');
+  }
+  decrypt(input, key) {
+    if (input === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    const inputArr = input.split('').map(item => (this.keys.indexOf(item.toUpperCase()) !== -1) ? this.keys.indexOf(item.toUpperCase()) : item);
+    const keyArr = key.split('').map(item => this.keys.indexOf(item.toUpperCase()));
+    const newDiffArr = [];
+    let keyCounter = 0;
+
+    for (let i = 0; i < inputArr.length; i++) {
+      if (typeof inputArr[i] !== 'number') {
+        newDiffArr[i] = inputArr[i];
+      } else {
+        if (keyCounter >= keyArr.length) {
+          keyCounter = 0;
+        }
+        let diff = inputArr[i] - keyArr[keyCounter];
+        if (diff < 0) {
+          diff = this.keys.length + diff;
+        }
+        newDiffArr[i] = diff;
+        keyCounter++;
+      }
+    }
+
+    const output = newDiffArr.map(item => (this.keys[item]) ? this.keys[item] : item);
+    return this.isdirect ? output.join('') : output.reverse().join('');
   }
 }
 
